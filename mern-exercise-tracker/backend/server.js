6878a66f4018b2a4910dfe68718e93d1a11e3c84
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-08-18 23:17:09 +0530

    Establishing connection and creation of moongose schema

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+//mongoose lets us connect with MongoDB database
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () =>
-{
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 });
