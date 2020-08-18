4538a295c38a34285952477e50bfe1bc52da22d2
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-08-19 00:49:49 +0530

    Added CRUD operation on Excercise

--- mern-exercise-tracker/backend/routes/excercises.js
+++ mern-exercise-tracker/backend/routes/excercises.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-let Exercise = require("../models/user.model");
+let Exercise = require("../models/exercise.model");
 
 router.route('/').get((req, res) => {
     Exercise.find()
-        .then(users => res.json(excercises))
+        .then(excercises => res.json(excercises))
         .catch(error => res.status(400).json('Error:' + error));
 });
 
@@ -26,4 +26,33 @@ router.route('/add').post((req, res) => {
         .catch(error => res.status(400).json('Error:' + error));
 });
 
+router.route('/:id').get((req, res) => {
+    console.log(req.params.id);
+    Exercise.findById(req.params.id)
+        .then(excercise => res.json(excercise))
+        .catch(error => res.status(400).json('Error:' + error));
+
+});
+
+router.route("/:id").delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Excercise deleted !"))
+        .catch(error => res.status(400).json('Error:' + error));
+});
+
+router.route("/update/:id").post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(excercise => {
+            excercise.username = req.body.username;
+            excercise.description = req.body.description;
+            excercise.duration = Number(req.body.duration);
+            excercise.date = Date.parse(req.body.date);
+
+            excercise.save()
+                .then(() => res.json("Succesfully updated excercise !"))
+                .catch(error => res.status(400).json('Error:' + error));
+
+        })
+        .catch(error => res.status(400).json('Error:' + error));
+});
 module.exports = router;
